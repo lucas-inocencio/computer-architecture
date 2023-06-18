87ab636045d1179a9ba06f1e07bd643826bedc58
lucas-inocencio <li.lucasinocencio@gmail.com>
2023-06-17 23:10:37 -0300

    AVX512 to AVX2

--- src/dgemm.c
+++ src/dgemm.c
@@ -5,7 +5,7 @@
 // uses single-dimensional versions of matrices, and and address arithmetic to get
 // better performance instead of using the more intuitive two-dimensional arrays that we saw in Python.
 
-#include <x86intrin.h>
+#include <immintrin.h>
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -36,21 +36,21 @@ void do_block1(int n, int si, int sj, int sk, double *A, double *B, double *C)
 void do_block2(int n, int si, int sj, int sk,
                double *A, double *B, double *C)
 {
-    for (int i = si; i < si + BLOCKSIZE; i += UNROLL * 8)
+    for (int i = si; i < si + BLOCKSIZE; i += UNROLL * 4)
         for (int j = sj; j < sj + BLOCKSIZE; j++)
         {
-            __m512d c[UNROLL];
+            __m256d c[UNROLL];
             for (int r = 0; r < UNROLL; r++)
-                c[r] = _mm512_load_pd(C + i + r * 8 + j * n);
+                c[r] = _mm256_load_pd(C + i + r * 4 + j * n);
             for (int k = sk; k < sk + BLOCKSIZE; k++)
             {
-                __m512d bb = _mm512_broadcastsd_pd(_mm_load_sd(B + j * n + k));
+                __m256d bb = _mm256_broadcast_sd(B + j * n + k);
                 for (int r = 0; r < UNROLL; r++)
-                    c[r] = _mm512_fmadd_pd(_mm512_load_pd(A + n * k + r * 8 + i), bb, c[r]);
+                    c[r] = _mm256_fmadd_pd(_mm256_load_pd(A + n * k + r * 4 + i), bb, c[r]);
             }
 
             for (int r = 0; r < UNROLL; r++)
-                _mm512_store_pd(C + i + r * 8 + j * n, c[r]);
+                _mm256_store_pd(C + i + r * 4 + j * n, c[r]);
         }
 }
 
@@ -68,13 +68,13 @@ void dgemm2(int n, double *A, double *B, double *C)
     }
 }
 
-void dgemm3(size_t n, double *A, double *B, double *C)
+void dgemm3(int n, double *A, double *B, double *C)
 {
-    for (size_t i = 0; i < n; i += 4)
-        for (size_t j = 0; j < n; j++)
+    for (int i = 0; i < n; i += 4)
+        for (int j = 0; j < n; j++)
         {
             __m256d c0 = _mm256_load_pd(C + i + j * n);
-            for (size_t k = 0; k < n; k++)
+            for (int k = 0; k < n; k++)
                 c0 = _mm256_add_pd(c0,
                                    _mm256_mul_pd(_mm256_load_pd(A + i + k * n),
                                                  _mm256_broadcast_sd(B + k + j * n)));
@@ -84,21 +84,21 @@ void dgemm3(size_t n, double *A, double *B, double *C)
 
 void dgemm4(int n, double *A, double *B, double *C)
 {
-    for (int i = 0; i < n; i += UNROLL * 8)
+    for (int i = 0; i < n; i += UNROLL * 4)
         for (int j = 0; j < n; ++j)
         {
-            __m512d c[UNROLL];
+            __m256d c[UNROLL];
             for (int r = 0; r < UNROLL; r++)
-                c[r] = _mm512_load_pd(C + i + r * 8 + j * n);
+                c[r] = _mm256_load_pd(C + i + r * 4 + j * n);
 
             for (int k = 0; k < n; k++)
             {
-                __m512d bb = _mm512_broadcastsd_pd(_mm_load_sd(B + j * n + k));
+                __m256d bb = _mm256_broadcast_sd(B + j * n + k);
                 for (int r = 0; r < UNROLL; r++)
-                    c[r] = _mm512_fmadd_pd(_mm512_load_pd(A + n * k + r * 8 + i), bb, c[r]);
+                    c[r] = _mm256_fmadd_pd(_mm256_load_pd(A + n * k + r * 4 + i), bb, c[r]);
             }
             for (int r = 0; r < UNROLL; r++)
-                _mm512_store_pd(C + i + r * 8 + j * n, c[r]);
+                _mm256_store_pd(C + i + r * 4 + j * n, c[r]);
         }
 }
 
@@ -139,7 +139,6 @@ void measureTime(void (*function)(int, double *, double *, double *), int n, dou
     printf("n = %d Time needed: %f seconds\n", n, cpu_time_used);
 }
 
-// create larger random matrices, call DGEMM and time it
 int main()
 {
     int n = 1000;
@@ -161,9 +160,9 @@ int main()
     measureTime(dgemm2, n, A, B, C);
     measureTime(dgemm3, n, A, B, C);
     measureTime(dgemm4, n, A, B, C);
-    measureTime(dgemm5_1, n, A, B, C);
-    measureTime(dgemm5_2, n, A, B, C);
-    measureTime(dgemm6, n, A, B, C);
+    //measureTime(dgemm5_1, n, A, B, C);
+    //measureTime(dgemm5_2, n, A, B, C);
+    //measureTime(dgemm6, n, A, B, C);
 
     return 0;
 }
